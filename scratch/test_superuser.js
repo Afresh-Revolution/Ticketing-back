a0b4dd34511d0d7c1234efe211560172a32a6d30
William <williambosworth420@gmail.com>
2026-05-22 12:17:33 +0100

    Will

--- scratch/test_superuser.js
+++ scratch/test_superuser.js
@@ -4,6 +4,7 @@ import { query } from '../src/shared/config/db.js';
   try {
     const res = await query(`SELECT * FROM "User" WHERE id = '0'`);
     console.log("Superuser:", res.rows);
+
   } catch (e) { 
     console.error(e); 
   }
